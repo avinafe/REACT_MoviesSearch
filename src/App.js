8010284de85db736394611062f6ed7fed594f806
Viñas Fernandez <alvs@gft.com>
2019-04-22 17:23:46 +0200

    220419_10-70 Enrutado básico

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import './App.css';
 import 'bulma/css/bulma.css';
 
 class App extends Component {
-  state = { results : []}
+  state = { usedSearch: false, results: []}
 
   _handleResults = results => {
-    this.setState({ results })
+    this.setState({ results, usedSearch: true })
   }
-  
+
+  _renderResults = () => {
+    return this.state.results.length === 0 
+      ? <p>Sorry! Results not found!</p>
+      : <MoviesList movies={this.state.results}/>
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,10 +25,11 @@ class App extends Component {
         <div className="SearchForm-wrapper">
           <SearchForm onResults={this._handleResults}/> 
         </div>
-        {this.state.results.length === 0 
-          ? <p>Sin resultados</p>
-          : <MoviesList movies={this.state.results}/>
+        {this.state.usedSearch 
+          ? this._renderResults()
+          : <small>Use the form to search a movie</small>
         }
+
       </div>
     );
   }
