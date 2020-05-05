7f7a21e89a3017a9623e754ceb12706885a21bb4
ANGEL VIÑAS FERNANDEZ <angel.vinas.contractor@bbva.com>
2020-05-05 16:15:58 +0200

    Page <Home /> optimized

--- src/App.js
+++ src/App.js
@@ -1,49 +1,50 @@
-import React, { Component } from 'react';
-import { Detail } from './Pages/Detail';
-import { MoviesList } from './Components/MoviesList';
-import { SearchForm } from './Components/SearchForm';
-import { Title } from './Components/Title';
-import './App.css';
-import 'bulma/css/bulma.css';
+import React, { Component } from "react";
+import { Home } from "./Pages/Home";
+import { Detail } from "./Pages/Detail";
+// import { MoviesList } from "./Components/MoviesList";
+// import { SearchForm } from "./Components/SearchForm";
+// import { Title } from "./Components/Title";
+import "./App.css";
+import "bulma/css/bulma.css";
 
 class App extends Component {
-  state = { usedSearch: false, results: []}
+	// state = { usedSearch: false, results: [] };
 
-  _handleResults = results => {
-    this.setState({ results, usedSearch: true })
-  }
+	// _handleResults = results => {
+	// 	this.setState({ results, usedSearch: true });
+	// };
 
-  _renderResults = () => {
-    return this.state.results.length === 0 
-      ? <p>Sorry! Results not found!</p>
-      : <MoviesList movies={this.state.results}/>
-  }
+	// _renderResults = () => {
+	// 	return this.state.results.length === 0 ? (
+	// 		<p>Sorry! Results not found!</p>
+	// 	) : (
+	// 		<MoviesList movies={this.state.results} />
+	// 	);
+	// };
 
-  render() {
+	render() {
+		const url = new URL(document.location);
+		const hasId = url.searchParams.has("id");
 
-    const url = new URL(document.location)
-    const hasId = url.searchParams.has('id')
+		if (hasId) {
+			return <Detail id={url.searchParams.get("id")} />;
+		}
 
-    if (hasId){
-      return <Detail id={url.searchParams.get('id')}/>
-    }
-  
-
-
-    return (
-      <div className="App">
-        <Title>Search Movies</Title>
-        <div className="SearchForm-wrapper">
-          <SearchForm onResults={this._handleResults}/> 
-        </div>
-        {this.state.usedSearch 
-          ? this._renderResults()
-          : <small>Use the form to search a movie</small>
-        }
-
-      </div>
-    );
-  }
+		return (
+			<div className="App">
+				<Home />
+				{/* <Title>Search Movies</Title>
+				<div className="SearchForm-wrapper">
+					<SearchForm onResults={this._handleResults} />
+				</div>
+				{this.state.usedSearch ? (
+					this._renderResults()
+				) : (
+					<small>Use the form to search a movie</small>
+				)} */}
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { MoviesList } from "../Components/MoviesList";
+import { SearchForm } from "../Components/SearchForm";
+import { Title } from "../Components/Title";
+
+export class Home extends Component {
+	state = { usedSearch: false, results: [] };
+
+	_handleResults = results => {
+		this.setState({ results, usedSearch: true });
+	};
+	_renderResults = () => {
+		return this.state.results.length === 0 ? (
+			<p>Sorry! Results not found!</p>
+		) : (
+			<MoviesList movies={this.state.results} />
+		);
+	};
+	render() {
+		return (
+			<div>
+				<Title>Search Movies</Title>
+				<div className="SearchForm-wrapper">
+					<SearchForm onResults={this._handleResults} />
+				</div>
+				{this.state.usedSearch ? (
+					this._renderResults()
+				) : (
+					<small>Use the form to search a movie</small>
+				)}
+			</div>
+		);
+	}
+}
