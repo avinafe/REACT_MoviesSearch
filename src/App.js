4f579ee6bec636dda3d6da8ede47aa9b9838c0c6
ANGEL VIÑAS FERNANDEZ <angel.vinas.contractor@bbva.com>
2020-05-06 11:43:26 +0200

    SPA added

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Detail } from "./Pages/Detail";
-// import { MoviesList } from "./Components/MoviesList";
-// import { SearchForm } from "./Components/SearchForm";
-// import { Title } from "./Components/Title";
 import "./App.css";
 import "bulma/css/bulma.css";
 
 class App extends Component {
-	// state = { usedSearch: false, results: [] };
-
-	// _handleResults = results => {
-	// 	this.setState({ results, usedSearch: true });
-	// };
-
-	// _renderResults = () => {
-	// 	return this.state.results.length === 0 ? (
-	// 		<p>Sorry! Results not found!</p>
-	// 	) : (
-	// 		<MoviesList movies={this.state.results} />
-	// 	);
-	// };
-
 	render() {
-		const url = new URL(document.location);
-		const hasId = url.searchParams.has("id");
-
-		if (hasId) {
-			return <Detail id={url.searchParams.get("id")} />;
-		}
-
 		return (
 			<div className="App">
-				<Home />
-				{/* <Title>Search Movies</Title>
-				<div className="SearchForm-wrapper">
-					<SearchForm onResults={this._handleResults} />
-				</div>
-				{this.state.usedSearch ? (
-					this._renderResults()
-				) : (
-					<small>Use the form to search a movie</small>
-				)} */}
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/detail/:movieId" component={Detail} />
+				</Switch>
 			</div>
 		);
 	}
--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -1,45 +1,52 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-const API_KEY = 'f338b7e3';
+const API_KEY = "f338b7e3";
 export class Detail extends Component {
-    state = { movie: {} }
-    
-    static propTypes = {
-        id: PropTypes.string,
-    }
-    
-    _goBack = (e) => {
-        window.history.back()
-    }
+	state = { movie: {} };
 
-    _fetchMovie= (id) => {
-        fetch(`http://www.omdbapi.com/?apikey=${ API_KEY }&i=${ id }
+	static propTypes = {
+		match: PropTypes.shape({
+			isExact: PropTypes.bool,
+			params: PropTypes.object,
+			path: PropTypes.string,
+			url: PropTypes.string
+		})
+	};
+
+	_goBack = e => {
+		window.history.back();
+	};
+
+	_fetchMovie = ({ id }) => {
+		fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}
         `)
-            .then( res => res.json() )
-            .then( movie => { 
-                console.log(movie , id) 
-                this.setState({ movie })
-            })
-    }
+			.then(res => res.json())
+			.then(movie => {
+				console.log(movie, id);
+				this.setState({ movie });
+			});
+	};
 
-    componentDidMount() {
-        const { id } = this.props;
-        this._fetchMovie( id )
-    }
-    
-    render(){
-        const { Title, Actors, Poster, Metascore, Plot } = this.state.movie
+	componentDidMount() {
+		console.log("props", this.props);
 
-        return (
-            <div>
-                <button onClick={this._goBack}>Go back</button>
-                <h1>{Title}</h1>
-                <img src={Poster} alt={Title}/>
-                <h3>{Actors}</h3>
-                <span>{Metascore}</span>
-                <p>{Plot}</p>
-            </div>
-        )
-    }
+		const { movieId } = this.props.match.params;
+		this._fetchMovie({ id: movieId });
+	}
+
+	render() {
+		const { Title, Actors, Poster, Metascore, Plot } = this.state.movie;
+
+		return (
+			<div>
+				<button onClick={this._goBack}>Go back</button>
+				<h1>{Title}</h1>
+				<img src={Poster} alt={Title} />
+				<h3>{Actors}</h3>
+				<span>{Metascore}</span>
+				<p>{Plot}</p>
+			</div>
+		);
+	}
 }
