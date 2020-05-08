33d5954b79e8100b69152447a30958132d56c022
ANGEL VIÑAS FERNANDEZ <angel.vinas.contractor@bbva.com>
2020-05-08 09:48:41 +0200

    <Movie /> and <MovieList /> refactor

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,33 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-export class Movie extends Component {
-	static propTypes = {
+export const Movie = ({ id, poster, title, year }) => {
+	Movie.propTypes = {
 		id: PropTypes.string,
 		poster: PropTypes.string,
 		title: PropTypes.string,
 		year: PropTypes.string
 	};
-	render() {
-		const { id, poster, title, year } = this.props;
-
-		return (
-			<Link to={`/detail/${id}`} className="card">
-				<div className="card-image">
-					<figure className="image">
-						<img src={poster} alt={title} />
-					</figure>
-				</div>
-				<div className="card-content">
-					<div className="media">
-						<div className="media-content">
-							<p className="title is-4">{title}</p>
-							<p className="subtitle is-6">{year}</p>
-						</div>
+	return (
+		<Link to={`/detail/${id}`} className="card">
+			<div className="card-image">
+				<figure className="image">
+					<img src={poster} alt={title} />
+				</figure>
+			</div>
+			<div className="card-content">
+				<div className="media">
+					<div className="media-content">
+						<p className="title is-4">{title}</p>
+						<p className="subtitle is-6">{year}</p>
 					</div>
 				</div>
-			</Link>
-		);
-	}
-}
+			</div>
+		</Link>
+	);
+};
--- src/Components/MoviesList.js
+++ src/Components/MoviesList.js
@@ -1,31 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Movie } from "./Movie";
 
-export class MoviesList extends Component {
-	static propTypes = {
+export const MoviesList = ({ movies }) => {
+	MoviesList.propTypes = {
 		movies: PropTypes.array
 	};
-
-	render() {
-		const { movies } = this.props;
-		console.log(movies);
-
-		return (
-			<div className="MoviesList">
-				{movies.map(movie => {
-					return (
-						<div key={movie.imdbID} className="MoviesList-item">
-							<Movie
-								id={movie.imdbID}
-								poster={movie.Poster}
-								title={movie.Title}
-								year={movie.Year}
-							/>
-						</div>
-					);
-				})}
-			</div>
-		);
-	}
-}
+	console.log(movies);
+	return (
+		<div className="MoviesList">
+			{movies.map(movie => {
+				return (
+					<div key={movie.imdbID} className="MoviesList-item">
+						<Movie
+							id={movie.imdbID}
+							poster={movie.Poster}
+							title={movie.Title}
+							year={movie.Year}
+						/>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
